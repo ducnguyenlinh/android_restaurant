72a3639e55340daf9e49892e61fb5a68b0b02252
ducnguyenlinh <duclinh0305@gmail.com>
2019-11-25 00:58:26 +0900

    userのmodelを作成する

---
+++ app/src/main/java/com/example/android_restaurant/Model/User.java
@@ -0,0 +1,11 @@
+package com.example.android_restaurant.Model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String fbid;
+    private String userPhone;
+    private String userName;
+    private String userAddress;
+}
